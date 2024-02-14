93d29530110d438b2d593ca253d4fd4c5104609f
namchailin <102152637+namchailin@users.noreply.github.com>
2024-02-14 15:48:50 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze I/1357. 뒤집힌 덧셈/뒤집힌 덧셈.cc
@@ -0,0 +1,51 @@
+/* 뒤집힌 덧셈 */
+// 변수 크기만큼 배열 선언하려면 동적할당
+// < 1차원일 경우 >
+// 자료형 *배열명 = new 자료형[크기];
+// delete[] 배열명;
+// < 2차원일 경우 >
+// 자료형 **배열명 = new 자료형*[세로크기];
+// for (int i=0;i<n;++i) { 배열명[세로크기] = new 자료형[가로크기]; }
+// for (int i=0;i<세로크기;++i) { delete[] 배열명[i]; } delete[] 배열명;
+
+// a = (조건) ? c : d;  // 삼항연산자
+
+// string str = "";  // str 초기화
+
+// str.size()   // 문자열 배열 크기
+// str = stoi(str)   // 문자열 정수형 변환
+// 문자열 말고 문자의 정수형 변환은 '0'만 빼주면 됨
+// str.erase(제거영역의 시작인덱스, 제거영역 길이)   // 문자열 일부 삭제
+#include <string>
+// abs()   // int형의 절댓값함수
+#include <cstdlib>
+// string input; getline(cin, input);  // 문자열 줄단위 입력
+#include <iostream>
+// v.size()  // 벡터 크기
+#include <vector>
+using namespace ::std;
+
+int rev(int x) {
+    int reversed = 0;
+    while (x >= 10) {
+        reversed += x % 10;
+        reversed *= 10;
+        x /= 10;
+    }
+    reversed += x;
+    return reversed;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int x, y;
+    cin >> x >> y;
+
+    int sum = rev(x) + rev(y);
+
+    cout << rev(sum) << endl;
+
+    return 0;
+}
